ff52a550672e13d7105a63577b14c94ea2b79c04
Alix <alix.fumoleau@gmail.com>
2016-11-26 00:34:44 +0100

    Edit the start script to launch influx as a docker container

--- start.sh
+++ start.sh
@@ -1,14 +1,22 @@
-forever stopall
+# Clean up
+sudo forever stopall
+
+# InfluxDB container
+sudo docker stop transistor-influxdb
+sudo docker rm transistor-influxdb
+mkdir $PWD/data
+mkdir $PWD/data/influxdb
+sudo docker run -d --name transistor-influxdb --volume=/var/influxdb:$PWD/data/influxdb -p 8083:8083 -p 8086:8086 solderra/armhf-influxdb
 
 # internet-status module
 cd internet-status
 npm install
-forever start update-internet-status.js
+sudo forever start update-internet-status.js
 cd ..
 
 # web-interface module
 cd web-interface
 npm install
 webpack
-forever start server.js
+sudo forever start server.js
 cd ..
