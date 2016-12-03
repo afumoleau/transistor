b397f76533ce233b0d11e3539e44c7255b7121c2
Alix <alix.fumoleau@gmail.com>
2016-12-03 20:28:05 +0100

    Fix docker volume binding

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ sudo docker stop transistor-influxdb
 sudo docker rm transistor-influxdb
 mkdir $PWD/data
 mkdir $PWD/data/influxdb
-sudo docker run -d --name transistor-influxdb --volume=/var/influxdb:$PWD/data/influxdb -p 8083:8083 -p 8086:8086 solderra/armhf-influxdb
+sudo docker run -d --name transistor-influxdb -v $PWD/data/influxdb:/var/lib/influxdb -p 8083:8083 -p 8086:8086 solderra/armhf-influxdb
 
 # internet-status module
 cd internet-status
