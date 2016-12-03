53de1059bf2ca246c1d5d06679f4fdcd1712cb84
Alix <alix.fumoleau@gmail.com>
2016-12-03 19:14:26 +0100

    Fix internet-status updater

--- internet-status/update-internet-status.js
+++ internet-status/update-internet-status.js
@@ -1,26 +1,45 @@
 var influx = require('influx');
 var fetch = require('node-fetch');
 var timers = require('timers');
+var _ = require('lodash');
 
-var db = new influx.InfluxDB({host: 'localhost', database:'transistor'});
+var config = {
+	dbHost: 'localhost',
+	dbName: 'transistor',
+	url: 'https://www.google.com',
+	delay: 30000,
+	measurement: 'internet'
+}
 
-timers.setInterval(fetchURL, 30000);
-fetchURL();
+var db = new influx.InfluxDB({host: config.dbHost, database:config.dbName});
+
+init(db).then(function() {
+	timers.setInterval(fetchURL, config.delay);
+	fetchURL();
+});
+
+function init(db) {
+	return db.getDatabaseNames().then(function(names) {
+		if(!_.includes(names, config.dbName)) {
+			return db.createDatabase(config.dbName);
+		}
+	});
+}
 
 function fetchURL() {
-    fetch('https://www.google.com')
-        .then(getStatusFromResponse)
-        .then(registerStatus)
-        .catch(function(err) {
-            registerStatus(0);
-        });
+	fetch(config.url)
+		.then(getStatusFromResponse)
+		.then(registerStatus)
+		.catch(function(err) {
+			registerStatus(0);
+		});
 }
 
 function getStatusFromResponse(res) {
-    return res.status === 200 ? 1 : 0;
+	return res.status === 200 ? 1 : 0;
 }
 
 function registerStatus(value) {
-    db.writePoints([{measurement:'internet', fields:{ value: value }}])
-        .catch(function(err) { console.log(err); });
+	db.writePoints([{measurement:config.measurement, fields:{ value: value }}])
+		.catch(function(err) { console.log(err); });
 }
