404772da87b0e9d8259635c25998fc9db6f437eb
Alix <alix.fumoleau@gmail.com>
2016-12-03 18:58:39 +0100

    Edit start script, remove broken support for x86_64

--- start.sh
+++ start.sh
@@ -7,11 +7,7 @@ sudo docker stop transistor-influxdb
 sudo docker rm transistor-influxdb
 mkdir $PWD/data
 mkdir $PWD/data/influxdb
-if $arch=="x86_64"
-    sudo docker run -d --name transistor-influxdb -v %cd%/data/influxdb:/var/lib/influxdb -p 8083:8083 -p 8086:8086 influxdb
-elif $arch=="armv7l"
-    sudo docker run -d --name transistor-influxdb --volume=/var/influxdb:$PWD/data/influxdb -p 8083:8083 -p 8086:8086 solderra/armhf-influxdb
-fi
+sudo docker run -d --name transistor-influxdb --volume=/var/influxdb:$PWD/data/influxdb -p 8083:8083 -p 8086:8086 solderra/armhf-influxdb
 
 # internet-status module
 cd internet-status
