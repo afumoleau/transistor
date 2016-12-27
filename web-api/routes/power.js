97897df106385839d35ffe88e82346c37e57cda2
Alix <alix.fumoleau@gmail.com>
2016-12-27 21:31:39 +0100

    add the get power endpoint

--- web-api/routes/power.js
+++ web-api/routes/power.js
@@ -24,8 +24,24 @@ function updatePower(req, res) {
 	return res.send('ok');
 }
 
+function getPower(interval, samples) {
+	var to = new Date();
+	var from = new Date(to - interval*samples);
+
+	return db.query(`SELECT MEAN(iInst) AS iInst, MEAN(indexHC) AS indexHC, MEAN(indexHP) AS indexHP, MEAN(pApp) AS pApp
+		FROM power WHERE time >= '${from.toISOString()}'
+		AND time <= '${to.toISOString()}'
+		GROUP BY time(${interval}ms)`);
+};
+
 module.exports = {
 	init(app) {
 		app.post('/power', updatePower);
+		app.get('/power', function(req, res) {
+			res.setHeader('Content-Type', 'application/json');
+			var interval = parseInt(req.query.interval) || (60*60*1000);
+			var samples = parseInt(req.query.samples) || (24);
+			getPower(interval, samples).then(res.json.bind(res));
+		})
 	}
 }
